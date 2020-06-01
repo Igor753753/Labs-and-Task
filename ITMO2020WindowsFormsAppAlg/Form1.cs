4785dc858345f6ff2a6ab709ab77c74f62760251
Igor <amfispb@yandex.ru>
2020-06-01 20:06:03 +0300

    test

---
+++ ITMO2020WindowsFormsAppAlg/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO2020WindowsFormsAppAlg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int a, b, c;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            {
+                try
+                {
+                    a = Convert.ToInt32(Text); //замена консольного ввода
+                    b = Convert.ToInt32(Text);
+                    int c = a / b;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
+}
